bbe41f38f6387623cf62018d889ca741538e7052
Shashwath K S <shashwathkukkunoor@outlook.com>
2025-03-25 20:45:53 +0530

    initial commit

---
+++ backend/src/main/java/in/snyce/gatepass/GatepassApplication.java
@@ -0,0 +1,13 @@
+package in.snyce.gatepass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatepassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatepassApplication.class, args);
+	}
+
+}
